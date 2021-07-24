584116238d375543bc3675aa0c6807afd2fd1489
jmkd3v <jmk@jmksite.dev>
2021-07-24 17:33:45 -0400

    Offloaded badge grabbing to server

--- script.js
+++ script.js
@@ -26,27 +26,6 @@ function generateBadgeElement(name, description, href, image) {
 	return baseElement;
 }
 
-// fetched from https://devforum.roblox.com/t/open-ugc-catalog-applications-now-open/798186 - may not be complete
-let ugcList = [
-	77554513, 96554028, 8132835, 1580739387, 30653871, 34485319, 1775060430,
-	36897775, 1414801789, 142028241, 329424041, 8256234, 46997221, 133805584,
-	573010593, 35227929, 7177677, 2431203181, 968948, 2539808676, 2654365144,
-	84872528, 2398773364, 1195018220, 7659381, 65035247, 10402080, 105835676,
-	1745885765, 381259634, 634449478, 26690888, 125026259, 115792575, 340337026,
-	2252503015, 80877763, 252574978, 58238421, 50149457, 52968820, 7781164,
-	350504593, 7858636, 1930073979, 39678533, 35814207, 103999578, 1020490017,
-	66808226, 50138143, 215042084, 4983547, 164638398, 1244016, 4900333,
-	438930461, 25461577, 125753905, 513415862, 6652514, 7198521, 69612307,
-	1882767190, 31822522, 180524806, 155179916, 153604099, 83855370, 273586245,
-	2040318, 32839980, 451471882, 20264954, 13461533, 97707058, 8707132, 75398267,
-	95773772, 1200831649, 123597957, 29562872, 4090655, 1761716413, 138957456,
-	43977205, 3405539, 19619414, 16983447, 35601765, 126959593, 4040663,
-	154007453, 210231230, 1528189706, 509715560, 5711308, 101009624, 312647891,
-	180242189, 4317782, 24390851, 412616677, 15125423, 61491604, 28276317,
-	1744203206, 189529155, 85429145, 112370430, 64706414, 259499138, 14607853,
-	104112300,
-];
-
 (async () => {
 	let badgeContainer = document.querySelector(
 		"#roblox-badges-container > .section-content > ul"
@@ -60,61 +39,19 @@ let ugcList = [
 		let userId = parseInt(
 			location.pathname.substring(7, location.pathname.length - 8)
 		);
-		let nameContainer = document.querySelector(".profile-name");
-		let userName = nameContainer.innerHTML;
-		console.log(userName)
 
-		console.log(userId);
+		let badgesRequest = await fetch("https://api.jmk.gg/badges/user/" + userId);
+		let badges = await badgesRequest.json();
+		badges = badges.badges;
 
-		let rolesUrl =
-			"https://groups.roblox.com/v2/users/" + userId + "/groups/roles";
-		console.log(rolesUrl);
-
-		let rolesRequest = await fetch(rolesUrl, {
-			method: "GET",
-		});
-		let rolesData = await rolesRequest.json();
-		console.log(rolesData);
-		rolesData = rolesData.data;
-		rolesData = Object.assign(
-			{},
-			...rolesData.map((x) => ({ [x.group.id]: x }))
-		);
-		console.log(rolesData);
-		
-		let inUgcProgram = ugcList.includes(userId);
-		
-		if (!inUgcProgram) {
-			let inUgcRequest = await fetch("https://catalog.roblox.com/v1/search/items?creatorName=" + userName + "&limit=10")
-			let inUgcData = await inUgcRequest.json();
-			inUgcData = inUgcData.data;
-			if (inUgcData && inUgcData.length > 1) {
-				inUgcProgram = true;
-			} 
-		}
-
-		if (inUgcProgram) {
+		for (let badge of badges) {
+			console.log(badge);
 			badgeContainer.appendChild(
 				generateBadgeElement(
-					"UGC Creator",
-					"This badge is awarded to users who are part of the Roblox User Generated Content program.",
-					"https://devforum.roblox.com/t/ugc-catalog-is-now-live/331405",
-					"https://cdn.glitch.com/c9aca3ea-4558-456b-8206-a8f36b94cd20%2FUGC.svg"
-				)
-			);
-		}
-		
-		if (													// if...
-			rolesData[2868472] &&								// the user is in the Interns group
-			(rolesData[2868472].role.rank == 106 || 			// and has a rank of 106, or...
-			rolesData[2868472].role.name == "Accelerator")		// a rank named "Accelerator"...
-		) {
-			badgeContainer.appendChild(
-				generateBadgeElement(
-					"Accelerator",
-					"This badge is awarded to users who are part of the Roblox Accelerator program.",
-					"https://devforum.roblox.com/t/roblox-2021-the-accelerator-program/997573",
-					"https://cdn.glitch.com/c9aca3ea-4558-456b-8206-a8f36b94cd20%2FAccelerator.svg"
+					badge.name,
+					badge.description,
+					badge.link,
+					badge.image
 				)
 			);
 		}
